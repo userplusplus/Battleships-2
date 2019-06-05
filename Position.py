ccbfe2fe9ea4a30cc582d17b9610240ea2f996ca
GittinInThere <awalletsdream@gmail.com>
2019-06-06 02:16:48 +1200

    Position Finished
    Creation of gameboard and position files. Position is completed. Positon has x, y and is_occupied values.

---
+++ Position.py
@@ -0,0 +1,50 @@
+#Position
+#Author : Dominic Martindale
+#Date: 6/06/2019
+#About: A constructor for creating an object with x and y co-ords
+
+class Position:
+
+    #Sets x and y values.
+    #Wether postion is occupied or not can be left blank
+    # the default is not occupied.
+    def __init__(self, x, y, is_occupied=None):
+        self.__x = x
+        self.__y = y
+        if is_occupied = None:
+            self.__is_occupied = False
+        else:
+            self.__is_occupied = is_occupied
+
+    @property #Getter for x
+    def x(self):
+        return self.__x
+ 
+    @x.setter #Setter for x, int only
+    def x(self, value):
+        if type(value) = int: #int checker
+            self.__x = value
+        else:
+            return print("A wrong x value was entered into a position")
+ 
+    @property #Getter for y
+    def y(self):
+        return self.__y
+ 
+    @y.setter  #Setter for y int only
+    def y(self, value):
+        if type(value) = int: #int checker
+            self.__y = value
+        else:
+            return print("A wrong x value was entered into a position")
+
+    @property #Getter for is_occupied
+    def is_occupied(self):
+        return self.__is_occupied
+ 
+    @y.setter  #Setter for is_occupied, bloolean only
+    def is_occupied(self, value):
+        if type(value) = bool:  #boolean checker
+            self.__is_occupied = value
+        else:
+            return print("A wrong is_occupied value was entered into a position")
